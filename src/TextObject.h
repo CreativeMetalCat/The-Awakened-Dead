da64974b8a10eb7adcfddd46b187f4045b38ab55
MetalCat <36876492+CreativeMetalCat@users.noreply.github.com>
2019-01-15 18:52:51 +1000

    Updated Text Object 15.01.2019

--- src/TextObject.h
+++ src/TextObject.h
@@ -19,13 +19,22 @@ public:
 		return Color;
 	}
 	sf::Text textObj;
-	TextObject(std::string text, sf::Font&font, sf::Color color, int size, bool useTextSize = true)
+	TextObject(sf::Vector2f pos,std::string text, sf::Font&font, sf::Color color, int size, bool useTextSize = true):Object(pos)
 	{
 		this->Text = text;
 		this->Color = color;
 		this->Size = size;
 
 		textObj = sf::Text(text, font, size);
+	}
+
+	TextObject(sf::Vector2f pos, std::string text, sf::Color color, sf::Text textObj, bool useTextSize = true):Object(pos),textObj(textObj)
+	{
+		this->Text = text;
+		this->Color = color;
+		this->Size = textObj.getCharacterSize();	
+		this->textObj.setString(text);
+		this->textObj.setPosition(pos);
 	}
 	virtual void OnCollision() {}
 	virtual void SetObjectPosition(sf::Vector2f pos) override
@@ -34,5 +43,27 @@ public:
 		collision.top = pos.y;
 
 		textObj.setPosition(pos);
+	}
+
+	void Init()override
+	{
+		this->textObj.setFillColor(Color);
+		textObj.setString(Text);
+	}
+
+	void Update(sf::Time dt)override
+	{
+		textObj.setString(Text);
+		this->collision = this->textObj.getGlobalBounds();
+	/*	this->textObj.setPosition(sf::Vector2f(this->collision.left, this->collision.left));*/
+	}
+
+	void Draw(sf::RenderWindow*&window)
+	{
+		window->draw(textObj);
+	}
+	~TextObject()
+	{
+		Text.~basic_string();
 	}
 };
