ffe59c8962a91e44e548c2539cea1a365d38d22a
MetalCat <36876492+CreativeMetalCat@users.noreply.github.com>
2019-01-16 17:35:55 +1000

    16.01.2019
    Added maps to repo

---
+++ maps/tad_tileset.tsx
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="tad_tileset" tilewidth="1057" tileheight="918" tilecount="25" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <terraintypes>
+  <terrain name="dev_blue" tile="20"/>
+  <terrain name="dev_brown" tile="26"/>
+  <terrain name="dev_green" tile="24"/>
+ </terraintypes>
+ <tile id="1">
+  <image width="64" height="64" source="../textures/map_tiles_tiles/concrete_floor1.jpg"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../textures/map_tiles_tiles/wood_floor.jpg"/>
+ </tile>
+ <tile id="3">
+  <image width="319" height="283" source="../textures/map_tiles_objects/objects_house_0008_Layer-9.png"/>
+ </tile>
+ <tile id="4">
+  <image width="197" height="215" source="../textures/map_tiles_objects/objects_house_0015_Layer-16.png"/>
+  <objectgroup draworder="index">
+   <object id="2" x="2" y="53" width="192" height="163"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <image width="1057" height="290" source="../textures/map_tiles_objects/objects_house_0058_Layer-59.png"/>
+ </tile>
+ <tile id="8">
+  <image width="256" height="206" source="../textures/map_tiles_objects/objects_house_0020_Layer-21.png"/>
+ </tile>
+ <tile id="9">
+  <image width="918" height="537" source="../textures/map_tiles_objects/objects_house_0047_Layer-48.png"/>
+ </tile>
+ <tile id="10">
+  <image width="631" height="918" source="../textures/map_tiles_objects/objects_house_0035_Layer-36.png"/>
+ </tile>
+ <tile id="11">
+  <image width="423" height="408" source="../textures/map_tiles_objects/objects_house_0055_Layer-56.png"/>
+ </tile>
+ <tile id="12">
+  <image width="313" height="206" source="../textures/survivor-move_rifle_0.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="../textures/map_tiles_tiles/metal_floor1.jpg"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="../textures/map_tiles_tiles/tiled_floor1.jpg"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="../textures/map_tiles_tiles/brick_floor1.jpg"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="64" source="../textures/map_tiles_tiles/brick_floor2.jpg"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="64" source="../textures/map_tiles_tiles/dirt1.jpg"/>
+ </tile>
+ <tile id="18">
+  <image width="64" height="64" source="../textures/map_tiles_tiles/grass1.jpg"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="../textures/dev/dev_blue_32x32.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="64" source="../textures/dev/dev_blue_64x64.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="../textures/dev/dev_brown_32x32.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="../textures/dev/dev_brown_64x64.png"/>
+ </tile>
+ <tile id="23" terrain=",,,1">
+  <image width="32" height="32" source="../textures/dev/dev_green_32x32.png"/>
+ </tile>
+ <tile id="24">
+  <image width="64" height="64" source="../textures/dev/dev_green_64x64.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="32" source="../textures/dev/dev_orange_32x32.png"/>
+ </tile>
+ <tile id="26" terrain="2,,2,">
+  <image width="64" height="64" source="../textures/dev/dev_orange_64x64.png"/>
+ </tile>
+ <tile id="28">
+  <image width="125" height="100" source="../textures/dev/dev_human_standart_100x125.png"/>
+ </tile>
+</tileset>
